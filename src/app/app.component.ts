6ba3d2508747ab21b065b1e053b2288ff7a26edb
Leroy Gerrits <leroygerrits@gmail.com>
2023-12-22 18:51:06 +0100

    Public shop by subdomain getters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output } from '@angular/core';
 
 import { MatDialog } from '@angular/material/dialog';
+import { PublicShop } from './shared/models/viewmodels/PublicShop.model';
+import { ShopService } from './shared/services/Shop.service';
 
 @Component({
   selector: 'app-root',
@@ -8,14 +10,25 @@ import { MatDialog } from '@angular/material/dialog';
 })
 
 export class AppComponent implements OnInit {
+  @Output() activeShop: PublicShop | undefined;
+
   public currentYear: number = new Date().getFullYear();
 
-  constructor(public dialog: MatDialog) { }
+  constructor(
+    private dialog: MatDialog,
+    private shopService: ShopService
+  ) { }
 
   ngOnInit(): void {
     const domain = window.location.host;
+    const subDomain = domain.replace('.dg', '');
+
+    this.shopService.getBySubdomainPublic(subDomain).subscribe(shop => this.activeShop = shop);
+
     console.log('domain');
     console.log(domain)
+    console.log('window.location');
+    console.log(window.location)
   }
 
   login() {
--- src/app/shared/services/Shop.service.ts
+++ src/app/shared/services/Shop.service.ts
@@ -1,19 +1,20 @@
-import { HttpClient, HttpParams } from '@angular/common/http';
-
 import { Environment } from 'src/app/shared/environments/Environment';
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { MutationResult } from 'src/app/shared/models/MutationResult';
 import { Observable } from 'rxjs';
 import { PublicShop } from '../models/viewmodels/PublicShop.model';
-import { Shop } from 'src/app/shared/models/Shop.model';
 
 @Injectable()
 export class ShopService {
-    private apiUrl = Environment.API_URL + '/Shop';
+    private apiUrl = Environment.API_URL + '/Shop/public';
 
     constructor(protected http: HttpClient) { }
 
-    getById(id: string): Observable<Shop> {
-        return this.http.get<Shop>(`${this.apiUrl}/${id}`);
+    getByIdPublic(id: string): Observable<PublicShop> {
+        return this.http.get<PublicShop>(`${this.apiUrl}/${id}`);
+    }
+
+    getBySubdomainPublic(subDomain: string): Observable<PublicShop> {
+        return this.http.get<PublicShop>(`${this.apiUrl}/GetBySubDomain/${subDomain}`);
     }
 }
